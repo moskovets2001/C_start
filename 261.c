f181d859f635c731a22c97a548427ad387a993b3
moskovets2001 <moskovets2001@yandex.ru>
2017-06-25 22:24:30 +0300

    1

---
+++ 261.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+//using namespace std;
+
+int main()
+{
+int a, b, c, h;
+
+
+
+scanf("%d", &a);
+scanf("%d", &b);
+scanf("%d", &c);
+scanf("%d", &h);
+
+if((c == a && h == b) || (a == 0 && b != 0)) { printf("NO");
+return 0;
+
+}
+if(a == 0 && b == 0) {printf("INF");
+return 0;
+}
+if (floor((abs((b - 2*b)/a) - 0.5)) == abs((b - 2*b)/a))
+{
+if(c*(b - 2*b)/a + h != 0)  printf((b - 2*b)/a);
+else  printf("NO");
+}
+else printf("NO");
+return 0;
+}
