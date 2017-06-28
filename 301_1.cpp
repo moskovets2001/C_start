bf47da84ecfda463433e0384534216972a189020
moskovets2001 <moskovets2001@yandex.ru>
2017-06-28 22:00:25 +0300

    исправил

--- 301_1.cpp
+++ 301_1.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <math.h>
-#define  EPS  00000.1
+#define  EPS  0000000.1
 using namespace std;
 
 int main()
@@ -12,16 +12,17 @@ double a, b, c, d, i, i1;
 cin >> a >> b >> c;
 
 d = b*b - 4*c*a;
+if(fabs(d) < EPS)
+{
+    cout << (b-2*b)/2*a;
+    return 0;
+}
 if(d < 0)
 {
  return 0;
 }
 
-if(d < EPS)
-{
-    cout << (b-2*b)/2*a;
-    return 0;
-}
+
 i = ((b - 2*b + sqrt(d))/(2*a));
 i1 = ((b - 2*b - sqrt(d))/(2*a));
 cout << i << endl << i1;
