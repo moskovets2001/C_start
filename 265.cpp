d1b4fbf5e15893727e10eb36c218835fec3c4930
moskovets2001 <moskovets2001@yandex.ru>
2017-06-29 21:13:49 +0300

    ok

--- 265.cpp
+++ 265.cpp
@@ -5,16 +5,21 @@ using namespace std;
 
 int main()
 {
-int k, m, n, stor, kolvo;
+    int k, m, n, stor, kolvo;
 
 
 
-cin >> k >> m >> n;
+    cin >> k >> m >> n;
 
-if(k == n){cout << 2*m; return 0;}
-stor = 2*n;
-if(stor % k == 0) cout << (stor/k)*m;
-else cout << ((stor/k)+1)*m;
-
-return 0;
+    if(k >= n)
+        {
+            cout << 2*m;
+            return 0;
+        }
+    stor = 2*n;
+    if(stor % k == 0)
+        cout << (stor/k)*m;
+    else
+        cout << ((stor/k)+1)*m;
+    return 0;
 }
