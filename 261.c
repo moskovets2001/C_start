c122f1b68086e2134e6d7c750815d585551430b2
moskovets2001 <moskovets2001@yandex.ru>
2017-06-29 14:50:42 +0300

    ispravil

--- 261.c
+++ 261.c
@@ -1,29 +1,30 @@
 #include <stdio.h>
 #include <math.h>
+#define  EPS  0000000.1
 //using namespace std;
 
 int main()
 {
 float a, b, c, h, o;
-
+#define  EPS  0000000.1
 
 
 scanf("%d", &a);
 scanf("%d", &b);
 scanf("%d", &c);
 scanf("%d", &h);
-
+    o = -b/a;
 if((c == a && h == b) || (a == 0 && b != 0)) { printf("NO");
 return 0;
 
 }
-if(a == 0 && b == 0) {printf("INF");
+if(fabs(a) < EPS && fabs(b) < EPS) {printf("INF");
 return 0;
 }
-if (floor((fabs((b - 2*b)/a) - 0.5)) == fabs((b - 2*b)/a))
+if (floor(fabs(o - 0.5)) == fabs(o))
 {
-    o = (b - 2*b)/a;
-if(c*(b - 2*b)/a + h != 0)  printf("%d", &o);
+
+if(c*o + h != 0)  printf("%d", &o);
 else  printf("NO");
 }
 else printf("NO");
