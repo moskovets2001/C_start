d222866240f39b5b9c2c5cb0ab66ac27c76bf2c4
moskovets2001 <moskovets2001@yandex.ru>
2017-07-02 19:32:47 +0300

    прошло

---
+++ 264.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+int main()
+{
+int x;
+
+
+
+scanf("%d", &x);
+while(x > 10) x = x-3;
+if(x == 10 || x == 9 || x == 8 || x == 3 || x == 5) printf("YES");
+else printf("NO");
+   return 0;
+}
