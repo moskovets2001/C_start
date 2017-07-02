0ac76bfb529de512387c121e68e20a616de816dc
moskovets2001 <moskovets2001@yandex.ru>
2017-07-02 19:55:10 +0300

    final version

--- 264.c
+++ 264.c
@@ -3,13 +3,13 @@
 
 int main()
 {
-int x;
-
-
-
-scanf("%d", &x);
-while(x > 10) x = x-3;
-if(x == 10 || x == 9 || x == 8 || x == 3 || x == 5) printf("YES");
-else printf("NO");
-   return 0;
+    int x;
+    scanf("%d", &x);
+    while (x > 10)
+        x = x - 3;
+    if (x == 10 || x == 9 || x == 8 || x == 3 || x == 5)
+        printf("YES");
+    else
+        printf("NO");
+    return 0;
 }
