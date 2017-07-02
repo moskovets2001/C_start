8259fa9ceafebe8d8540d569219822ed2c623e7c
moskovets2001 <moskovets2001@yandex.ru>
2017-07-02 19:49:42 +0300

    final version

--- 304_1.c
+++ 304_1.c
@@ -4,24 +4,22 @@
 int main()
 {
     int a = 0, b = 0, c = 0, n, d = 0;
-
-
-
     scanf("%d", &n);
-a = n/60;
-b = n%60;
-if(b >= 35) a++;
-else
+    a = n / 60;
+    b = n % 60;
+    if (b >= 35)
+        a++;
+    else
     {
-      c = b/10;
-      if(b%10 == 9)  c++;
-      else
+        c = b / 10;
+        if (b % 10 == 9)
+            c++;
+        else
         {
-         d = b%10;
+            d = b % 10;
         }
-
     }
 
-printf("%d %d %d", d, c, a);
-   return 0;
+    printf("%d %d %d", d, c, a);
+    return 0;
 }
