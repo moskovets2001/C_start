f632457750d4997068fba03f6276ff4a535bd364
moskovets2001 <moskovets2001@yandex.ru>
2017-07-02 19:22:17 +0300

    прошло

---
+++ 1445.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+int main()
+{
+int x, y, m, n;
+
+
+
+scanf("%d %d", &m, &n);
+scanf("%d %d", &x, &y);
+if(y-1 >= 1) printf("%d %d\n",  x, y-1);
+if(x-1 >= 1) printf("%d %d\n",  x-1, y);
+if(y+1 <= n) printf("%d %d\n",  x, y+1);
+if(x+1 <= m) printf("%d %d\n",  x+1, y);
+
+   return 0;
+}
