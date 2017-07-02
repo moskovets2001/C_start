d1ee3d20bf1414d29d0c2f550eb5c3cb98190ff0
moskovets2001 <moskovets2001@yandex.ru>
2017-07-02 19:59:32 +0300

    final version

--- 1459.c
+++ 1459.c
@@ -3,65 +3,62 @@
 
 int main()
 {
-int x;
+    int x;
+    scanf("%d", &x);
+    if (x == 100)
+    {
+        printf("C");
+        x = x - 100;
+    }
+    if (x / 10 == 9)
+    {
+        printf("XC");
+        x = x - 90;
+    }
 
 
+    while (x >= 50)
+    {
+        printf("L");
+        x = x - 50;
+    }
+    if (x / 10 == 4)
+    {
+        printf("XL");
+        x = x - 40;
+    }
 
-scanf("%d", &x);
-if(x == 100)
-{
-    printf("C");
-   x = x-100;
-}
-if(x/10 == 9)
-{
- printf("XC");
- x  = x-90;
-}
+    while (x >= 10)
+    {
+        printf("X");
+        x = x - 10;
+    }
 
+    if (x % 10 == 6)
+    {
+        printf("VI");
+        x = x - 6;
+    }
+    if (x % 10 == 9)
+    {
+        printf("IX");
+        x = x - 9;
+    }
+    while (x >= 5)
+    {
+        printf("V");
+        x = x - 5;
+    }
+    if (x % 10 == 4)
+    {
+        printf("IV");
+        x = x - 4;
+    }
+    while (x >= 1)
+    {
+        printf("I");
+        x--;
+    }
 
-while(x >= 50)
-{
-    printf("L");
-    x = x - 50;
-}
-if(x/10 == 4)
-{
- printf("XL");
- x  = x-40;
-}
-
-while(x >= 10)
-{
-    printf("X");
-    x = x - 10;
-}
-
-if(x%10 == 6)
-{
- printf("VI");
- x  = x-6;
-}
-if(x%10 == 9)
-{
- printf("IX");
- x  = x-9;
-}
-while(x >= 5)
-{
-    printf("V");
-    x = x - 5;
-}
-if(x%10 == 4)
-{
- printf("IV");
- x  = x-4;
-}
-while(x >= 1)
-{
-    printf("I");
-    x--;
-}
-
-   return 0;
+    return 0;
 }
