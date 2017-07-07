7e3e7b63a69bf842f4994bb38963c7f200280af0
moskovets2001 <moskovets2001@yandex.ru>
2017-07-07 19:43:57 +0300

    ок

---
+++ 3077.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+
+
+int main()
+{
+    int n, f, k = 0, max = 1;
+    scanf("%d", &n);
+    while (n != 0)
+    {
+        f = n;
+        scanf("%d", &n);
+        if (f == n)
+        {
+            k++;
+            while (f == n)
+            {
+                f = n;
+                scanf("%d", &n);
+                k++;
+            }
+        }
+        if (k > max)
+            max = k;
+        k = 0;
+    }
+    printf("%d", max);
+    return 0;
+}
+
