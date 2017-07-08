f5fdc36dd2c8c3c9d128a24009f0d3ecfb138432
moskovets2001 <moskovets2001@yandex.ru>
2017-07-08 16:22:17 +0300

    исправил

--- 317.c
+++ 317.c
@@ -5,16 +5,14 @@
 
 int main()
 {
-    int  n, k, c, k1 = 1, n1 = 1, i, q = 1;
+    int  n, k, c, n1 = 1, i, q = 1;
 
     scanf("%d%d", &n, &k);
-    for(i = 1; i <= n; i++)
+    for(i = k; i <= n; i++)
         n1 *= i;
-    for(i = 1; i <= k; i++)
-        k1 *= i;
     for(i = 1; i <= (n - k); i++)
         q *= i;
-    c = n1/(k1 * q);
+    c = n1/q;
     printf("%d", c);
     return 0;
 
