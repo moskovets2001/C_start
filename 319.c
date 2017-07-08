75a2fd4331214d71fa1386970c3e66375f3675fa
moskovets2001 <moskovets2001@yandex.ru>
2017-07-08 19:06:12 +0300

    исправил

--- 319.c
+++ 319.c
@@ -8,18 +8,18 @@ int main()
     double a, a1, i, c = 0;
     double n;
     scanf("%lf%lf", &a, &n);
-    a1 = a;
+    a1 = 1;
     if(n == 0)
     {
         printf("1");
         return 0;
     }
-    for(i = 0; i < n-1; i++)
+    for(i = 0; i < n; i++)
     {
         a1 *= a;
         c += a1;
     }
-    printf("%lf", c + a + 1);
+    printf("%lf", c + 1);
     return 0;
 
 }
