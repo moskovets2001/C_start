c576b1cca0be7242319c1cf41ad3fd063e3b7713
moskovets2001 <moskovets2001@yandex.ru>
2017-07-08 19:17:27 +0300

    исправил

--- 3077.c
+++ 3077.c
@@ -6,23 +6,20 @@ int main()
 {
     int n, f, k = 0, max = 1;
     scanf("%d", &n);
-    while (n != 0)
-    {
         f = n;
         scanf("%d", &n);
         k++;
-        while (f == n)
+        while (f == n || n != 0)
         {
             f = n;
             scanf("%d", &n);
             k++;
+            if (k > max)
+                max = k;
+            k = 0;
         }
 
-        if (k > max)
-            max = k;
-        k = 0;
-    }
-    printf("%d", max);
+    printf("%d", max+1);
     return 0;
 }
 
